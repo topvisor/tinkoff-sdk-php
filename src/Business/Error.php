5f75deb0d1232082f1f70bc70383ce99be468e46
anton <anton@topvisor.com>
2021-01-12 19:08:39 +0300

    v0.5

--- src/Business/Error.php
+++ src/Business/Error.php
@@ -8,6 +8,10 @@ use Throwable;
 
 class Error extends \Exception {
 
+	public function __construct($message = "", $code = 0, Throwable $previous = null){
+		parent::__construct($message, $code, $previous);
+	}
+
 	public int $statusCode;
 	public ?string $xRequestId;
 	public ?string $id;
