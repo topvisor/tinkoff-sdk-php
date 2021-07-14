235ddc016482dd62101fba27ace45ce30ca1bc88
anton <anton@topvisor.com>
2021-07-14 12:48:17 +0300

    v0.13 - выставление счета

--- src/Id/AuthToken.php
+++ src/Id/AuthToken.php
@@ -5,17 +5,17 @@ namespace Topvisor\TinkoffSdk\Id;
 class AuthToken {
 
 	public string $accessToken;
-	public int $expires;
+	public ?int $expires;
 	public ?string $refreshToken;
 
-	public function __construct(string $accessToken, int $expires, ?string $refreshToken = NULL){
+	public function __construct(string $accessToken, ?int $expires, ?string $refreshToken = NULL){
 		$this->accessToken = $accessToken;
 		$this->expires = $expires;
 		$this->refreshToken = $refreshToken;
 	}
 
 	public function isExpired(): bool {
-		if ($this->expires > time()) {
+		if (is_null($this->expires) || $this->expires > time()) {
 			$isExpired = false;
 		} else {
 			$isExpired = true;
