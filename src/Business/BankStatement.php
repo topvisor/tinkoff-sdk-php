ea9343bccea6c824da5645c87c6ba1fdb2cac864
decaseal <decaseal@gmail.com>
2022-03-25 10:18:50 +0300

    Update BankStatement.php

--- src/Business/BankStatement.php
+++ src/Business/BankStatement.php
@@ -5,10 +5,10 @@ namespace Topvisor\TinkoffSdk\Business;
 class BankStatement {
 
 	public string $accountNumber;
-	public int $saldoIn;
-	public int $income;
-	public int $outcome;
-	public int $saldoOut;
+	public float $saldoIn;
+	public float $income;
+	public float $outcome;
+	public float $saldoOut;
 	public ?array $operation = NULL;
 
 }
